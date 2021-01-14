5fea1d12da1e5be28bc817a319e229e86582d99c
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-01-14 11:34:48 -0600

    base

---
+++ public/js.js
@@ -0,0 +1,67 @@
+var form = document.getElementById("form");
+var txtMensaje = document.getElementById("txtMensaje")
+var ulMensajes = document.getElementById('ulMensajes') 
+var ulUsuarios = document.getElementById('ulUsuarios') 
+var lblNombreUsuario = document.getElementById('lblNombreUsuario') 
+var socket = io.connect('http://10.0.93.123:3000');
+var nombreUsuario = ""
+
+//console.log(txtMensaje)
+
+//ulMensajes.appendChild("<li>Chat Iniciado</li>")
+
+while(nombreUsuario.trim() == ""){
+  nombreUsuario = prompt("Ingrese un nombre de usuario")
+
+  if(nombreUsuario.trim() == ""){
+    alert("Debe ingresar un nombre de usuario")
+  }
+}
+
+socket.emit("inicioSesion", nombreUsuario);
+
+lblNombreUsuario.textContent = "Nombre de Usuario: " + nombreUsuario
+
+socket.on('mensaje', function (mensaje) {
+    var li = document.createElement("li");
+    li.innerHTML = mensaje;
+    ulMensajes.appendChild(li)
+});
+
+socket.on('inicioSesion', function (mensaje) {
+
+  ulUsuarios.innerHTML = ""
+
+  var usuarios = JSON.parse(mensaje)
+
+  usuarios.forEach(usuario => {
+    var li = document.createElement("li");
+    li.innerHTML = usuario;
+    ulUsuarios.appendChild(li)
+  });
+});
+
+socket.on('cerrarSesion', function (mensaje) {
+  ulUsuarios.innerHTML = ""
+
+  var usuarios = JSON.parse(mensaje)
+
+  usuarios.forEach(usuario => {
+    var li = document.createElement("li");
+    li.innerHTML = usuario;
+    ulUsuarios.appendChild(li)
+  });
+});
+
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    socket.emit("mensaje", new Date().toLocaleString("es-ES") + " - " + "<span style='color:blue;'>" + nombreUsuario + "</span>: " + txtMensaje.value);
+    txtMensaje.value = ""
+
+    return false;
+}, false);
+
+function cerrarSesion(){
+  socket.emit("cerrarSesion", nombreUsuario);
+}
---
+++ server/index.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var app = express()
+var server = require("http").Server(app);
+var port = process.env.PORT || 3000;
+var io = require('socket.io')(server, { origins: '*:*'});
+var usuariosConectados = [];
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res) {
+    console.log("Server iniciado");
+});
+
+server.listen(port, function() {
+    console.log("Escuchando en el puerto (socket): " + port);
+});
+
+io.on("connection", function(socket){
+
+    console.log("socket")
+    socket.on("inicioSesion", function(mensaje){
+        usuariosConectados.push(mensaje)
+        io.emit("inicioSesion", JSON.stringify(usuariosConectados))
+        console.log("Usuario Conectado: " + mensaje)
+    });
+
+    socket.on("cerrarSesion", function(mensaje){
+        for(contador = 0; contador < usuariosConectados.length; contador++){
+            if(usuariosConectados[contador] == mensaje){
+                console.log("Usuario Desconectado: " + usuariosConectados[contador])
+                usuariosConectados.splice(contador, 1)
+                contador = usuariosConectados.length
+            }
+        }
+
+        io.emit("cerrarSesion", JSON.stringify(usuariosConectados))
+    });
+
+    socket.on("mensaje", function(mensaje){
+        io.emit("mensaje", mensaje)
+    });
+
+    socket.on("disconnect", function(){
+        console.log("Usuario desconectado")
+    })
+})
